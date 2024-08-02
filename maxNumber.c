0c8d62fbb4d92133c4955c9550e718e3ac181e23
lilyderiv <busrakubradereli@gmail.com>
2024-08-02 23:11:01 +0300

    En buyuk sayi sorusu eklendi.

---
+++ maxNumber.c
@@ -0,0 +1,31 @@
+/*SORU: EN BUYUK SAYI
+
+Kullanicidan uc tane tam sayý aliniz. Alinan tam sayilar arasindaki en buyuk sayiyi bulunuz.
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2, num3;
+    int maxNumber;
+    printf("Uc sayi giriniz: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    if (num1 > num2){
+        maxNumber = num1;
+        if(num1 < num3){
+            maxNumber = num3;
+        }
+    }
+    else{
+        maxNumber = num2;
+        if(num2 < num3){
+            maxNumber = num3;
+        }
+    }
+    printf("Girdiginiz sayilarin en buyugu %d ", maxNumber);
+    return 0;
+}
